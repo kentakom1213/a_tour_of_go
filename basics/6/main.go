6187fd2788e70456b85f37eaaae27fed75bbfef7
kentakom1213 <kentakom1213@gmail.com>
2025-06-25 23:36:36 +0900

    add: ファイル作成など

---
+++ basics/6/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func swap(x, y string) (string, string) {
+	return y, x
+}
+
+func main() {
+	a, b := "Kenta", "KOMOTO"
+	fmt.Println(a, b)
+
+	a, b = swap(a, b)
+	fmt.Println(a, b)
+}
